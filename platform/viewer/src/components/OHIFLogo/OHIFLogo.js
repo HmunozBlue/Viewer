c591ea1105d47e1a74d468373a268310b7538ac6
HmunozBlue <hmunoz@mibluemedical.com>
2019-10-29 12:03:39 -0600

    Visualizador Blue

---
+++ platform/viewer/src/components/OHIFLogo/OHIFLogo.js
@@ -0,0 +1,19 @@
+import './OHIFLogo.css';
+
+import { Icon } from '@ohif/ui';
+import React from 'react';
+
+function OHIFLogo() {
+  return (
+    <a
+    /*target="_blank"
+    rel="noopener noreferrer"
+    className="header-brand"
+    href="https://www.mibluemedical.com/gt/"*/
+    >
+      <img src="assets/BlueLogo.png" width="300" height="120" />
+    </a>
+  );
+}
+
+export default OHIFLogo;
